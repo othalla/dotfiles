9b7af458b861d534c9c62b29184a307e53693086
Florian Chardin <othalla.lf@gmail.com>
2022-12-12 21:29:52 +0100

    Add nvim config with chezmoi

---
+++ dot_config/nvim/config.lua
@@ -0,0 +1,266 @@
+-- lualine config
+-- ==============
+require('lualine').setup{
+	options = {
+		theme = 'tokyonight',
+
+		extensions = { 'fugitive' },
+	}
+}
+
+-- ===========
+-- LSP configs
+-- ===========
+--
+local lspconfig = require('lspconfig')
+local configs = require 'lspconfig/configs'
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+	--Enable completion triggered by <c-x><c-o>
+	buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	local opts = { noremap=true, silent=true }
+
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+	buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.show_line_diagnostics()<CR>', opts)
+	buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+	buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+	buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+end
+
+lspconfig.golangci_lint_ls.setup {
+	on_attach = on_attach,
+	filetypes = {'go'},
+	root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+	init_options = {
+		command = { "golangci-lint", "run", "--out-format", "json" };
+	}
+}
+
+lspconfig.gopls.setup {
+	on_attach = on_attach,
+	root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+	cmd = {"gopls", "serve"},
+	settings = {
+		gopls = {
+			gofumpt = true,
+			staticcheck = true,
+			codelenses = {
+				generate = false,
+				gc_details = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = true,
+			}
+		},
+	},
+}
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		end,
+	},
+	mapping = {
+		['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-y>'] = cmp.config.disable,
+		['<C-e>'] = cmp.mapping({
+			i = cmp.mapping.abort(),
+			c = cmp.mapping.close(),
+		}),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'vsnip' }, -- For vsnip users.
+	}, {
+		{ name = 'buffer' },
+		{ name = 'path'},
+	})
+})
+
+-- Use buffer source for `/`.
+cmp.setup.cmdline('/', {
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(':', {
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
+
+-- =================
+-- Treesitter config
+-- =================
+--
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = "all",
+	highlight = {
+		enable = true,
+	},
+}
+
+local lspkind = require('lspkind')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol',
+      maxwidth = 50,
+    })
+  }
+}
+
+-- ======
+-- Golang
+-- ======
+
+-- Scrollbar with LSP
+local colors = require("tokyonight.colors").setup()
+
+require("scrollbar").setup({
+    handle = {
+        color = colors.bg_highlight,
+    },
+    marks = {
+        Search = { color = colors.orange },
+        Error = { color = colors.error },
+        Warn = { color = colors.warning },
+        Info = { color = colors.info },
+        Hint = { color = colors.hint },
+        Misc = { color = colors.purple },
+    }
+})
+
+require('spellsitter').setup {
+  enable = true,
+}
+
+require('go').setup({
+  max_line_len = 140,
+})
+
+vim.api.nvim_exec([[ autocmd BufWritePre (InsertLeave?) <buffer> lua vim.lsp.buf.formatting_sync(nil,500) ]], false)
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+
+require('dap-go').setup()
+
+require("dapui").setup({
+  icons = { expanded = "", collapsed = "", current_frame = "" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+    toggle = "t",
+  },
+  -- Use this to override mappings for specific elements
+  element_mappings = {},
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  -- expand_lines = vim.fn.has("nvim-0.7") == 1,
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+      -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.85 },
+        { id = "breakpoints", size = 0.15 },
+      },
+      size = 15, -- 40 columns
+      position = "bottom",
+    },
+  },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "",
+      run_last = "",
+      terminate = "",
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+    max_value_lines = 100, -- Can be integer or nil.
+  }
+})
+
+require("neotest").setup({
+	adapters = {
+		require("neotest-go")({
+			experimental = {
+				test_table = true,
+			},
+			args = { "-count=1", "-timeout=60s" }
+		})
+	},
+	icons = {
+		running = "?",
+	},
+	floating = {
+			border = "rounded",
+			max_height = 0.99,
+			max_width = 0.99,
+			options = {}
+		},
+	diagnostic = {
+		enabled = false,
+	},
+})
---
+++ dot_config/nvim/init.vim
@@ -0,0 +1,178 @@
+set encoding=UTF-8
+set mouse=
+
+filetype on
+filetype plugin on
+filetype indent on
+
+set termguicolors
+
+call plug#begin('~/.vim/plugged')
+" Plugin manager
+Plug 'VundleVim/Vundle.vim'
+" Git integration
+Plug 'tpope/vim-fugitive'
+Plug 'lewis6991/spellsitter.nvim'
+" Golang
+Plug 'ray-x/go.nvim'
+Plug 'ray-x/guihua.lua'
+" Comment
+Plug 'scrooloose/nerdcommenter'
+" Theme
+Plug 'folke/tokyonight.nvim', { 'branch': 'main' }
+" Icons for LSP
+Plug 'onsails/lspkind-nvim'
+" Custor line
+Plug 'yamatsum/nvim-cursorline'
+" helpers
+Plug 'godlygeek/tabular'
+Plug 'raimondi/delimitmate'
+Plug 'nvim-lua/popup.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+" LSP
+Plug 'neovim/nvim-lspconfig'
+" Completion
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+"compinit Statusbar
+Plug 'hoob3rt/lualine.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+" Treesitter
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+" ultest
+Plug 'voldikss/vim-floaterm'
+Plug 'antoinemadec/FixCursorHold.nvim'
+Plug 'nvim-neotest/neotest'
+Plug 'nvim-neotest/neotest-go'
+
+Plug 'romgrk/barbar.nvim'
+" Debugger
+Plug 'mfussenegger/nvim-dap'
+Plug 'theHamsta/nvim-dap-virtual-text'
+Plug 'rcarriga/nvim-dap-ui'
+Plug 'leoluz/nvim-dap-go'
+" ScrollBar
+Plug 'petertriho/nvim-scrollbar'
+Plug 'towolf/vim-helm'
+
+call plug#end()
+
+" Lua config
+" ==========
+luafile ~/.config/nvim/config.lua
+
+set completeopt=menu,menuone,noselect
+
+" leader
+let mapleader=";"
+"
+" nerdcommenter
+let g:NERDSpaceDelims = 1
+
+let g:tokyonight_style = "night"
+colorscheme tokyonight
+
+syntax enable
+filetype plugin indent on
+
+set listchars=eol:$,tab:>-,trail:.,extends:>,precedes:<
+set t_Co=256
+
+set nu
+set ruler
+set list
+set showmatch
+set hlsearch
+set nocompatible
+set wildmenu
+set incsearch
+
+" Default Indent & linesize
+set tabstop=2
+set shiftwidth=2
+set softtabstop=2
+set textwidth=100
+
+func! DeleteTrailingWS()
+  exe "normal mz"
+  %s/\s\+$//ge
+  exe "normal `z"
+endfunc
+
+func! DeleteNewLineEndOfFile()
+  set binary
+  set noeol
+endfunc
+
+" GO
+au FileType go set noexpandtab
+au FileType go set shiftwidth=4
+au FileType go set softtabstop=4
+au FileType go set tabstop=4
+
+" Go
+autocmd BufWrite *.go :call DeleteTrailingWS()
+autocmd Filetype python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=140 expandtab autoindent colorcolumn=140 fileformat=unix
+
+" Python
+autocmd BufWrite *.py :call DeleteTrailingWS()
+autocmd Filetype python setlocal tabstop=4 shiftwidth=4 softtabstop=4 textwidth=80 expandtab autoindent colorcolumn=88 fileformat=unix
+autocmd FileType python set omnifunc=pythoncomplete#Complete
+
+" Puppet
+au BufNewFile,BufRead *.pp set filetype=puppet
+autocmd BufWrite *.pp :call DeleteTrailingWS()
+autocmd Filetype puppet setlocal tabstop=2 shiftwidth=2 softtabstop=2 textwidth=80 expandtab autoindent colorcolumn=80 fileformat=unix
+
+au BufNewFile,BufRead *.eyaml set filetype=yaml
+autocmd BufWrite *.eyaml :call DeleteTrailingWS()
+" c
+autocmd FileType c setlocal shiftwidth=8 tabstop=8 softtabstop=8 textwidth=140
+autocmd BufWrite *.c :call DeleteTrailingWS()
+
+set foldmethod=expr
+set foldminlines=0
+set foldnestmax=3
+set foldlevelstart=20
+set foldexpr=nvim_treesitter#foldexpr()
+
+" Line Break
+:nnoremap <NL> i<CR><ESC>
+
+" Rename current word
+:nnoremap <Leader>s :%s/\<<C-r><C-w>\>/
+
+" Find files using Telescope command-line sugar.
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+" Using lua functions
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+" DAP
+nnoremap <leader>db <cmd>lua require'dap'.toggle_breakpoint()<cr>
+nnoremap <leader>dc <cmd>lua require'dap'.continue()<cr>
+nnoremap <leader>ds <cmd>lua require("dapui").setup()
+nnoremap <leader>dc <cmd>lua require("dapui").close()
+
+" Ultest mappings
+nmap tf <cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>
+nmap tn <cmd>lua require("neotest").run.run()<cr>
+nmap ts <cmd>lua require("neotest").summary.toggle()<cr>
+nmap to <cmd>lua require("neotest").output.open({ enter = false })<cr>
+
+" Dap mapping for tests
+nmap td <cmd>lua require('dap-go').debug_test()<cr>
+nmap ti <cmd>lua require("dapui").float_element()<cr>
+
+nnoremap <silent>[t <cmd>lua require("neotest").jump.prev({ status = "failed" })<CR>
+nnoremap <silent>]t <cmd>lua require("neotest").jump.next({ status = "failed" })<CR>
