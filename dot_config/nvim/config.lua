31fb032fe64ff0a56c99bd7e14c7c5579ced0169
Florian Chardin <othalla.lf@gmail.com>
2022-12-22 14:34:16 +0100

    Include hidden files in telescope

--- dot_config/nvim/config.lua
+++ dot_config/nvim/config.lua
@@ -8,6 +8,16 @@ require('lualine').setup{
 	}
 }
 
+require("telescope").setup {
+    pickers = {
+        live_grep = {
+            additional_args = function(opts)
+                return {"--hidden"}
+            end
+        },
+    },
+}
+
 -- ===========
 -- LSP configs
 -- ===========
